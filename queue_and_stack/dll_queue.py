af5074b97e8c5cd229ab8b25bc4448d5bdc55ea7
Frank Martinez <frankie.martinez@gmail.com>
2020-01-28 14:34:08 -0800

    ✨ stacks and queue mvp

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -9,17 +9,64 @@ from doubly_linked_list import DoublyLinkedList
 # len returns the number of items in the queue.
 
 
+# # Simple example from g4g
+# # Function to initialise the node object
+# class Node:
+#     def __init__(self, data):
+#         self.data = data    # Assign data
+#         self.next = None    # Initialize next as null
+# # Linked List class contains a Node object
+# class LinkedList:
+#     # Function to initialize head
+#     def __init__(self):
+#         self.head = None
+
+
 class Queue:
     def __init__(self):
+        # Start size at 0
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Import linked list
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # Add to head, increase size, return size
+        self.storage.add_to_head(value)
+        self.size += 1
+        return self.size
 
     def dequeue(self):
-        pass
+        # Check to see if the list length is less than or 0
+        # If zero, can't allow dequeue
+        if self.size <= 0:
+            return None
+        # Decrease size by 1
+        self.size -= 1
+        return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
+
+
+# Store the class in a variable
+# All the cool kids are doing it
+q = Queue()
+###################################
+# # 10
+# # head
+# # 10 <-> 11
+# # tail   head
+# # 10 <-> 11 <-> 12
+# # tail          head
+# # 10 <-> 11 <-> 12 <-> 13
+# # tail                 head
+# # 11 <-> 12 <-> 13
+# # tail          head
+# # 12 <-> 13
+# # tail   head
+###################################
+print(q.enqueue(10), q.enqueue(11), q.enqueue(12), q.enqueue(13))
+print(q.dequeue())  # Results in 10, the tail removed
+print(q.dequeue())  # Results in 10, the tail removed
+print(q.len())  # 2
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,31 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_head(value)
+        return self.size
 
     def pop(self):
-        pass
+        if self.size <= 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
+
+
+stk = Stack()
+print(stk.push(11), stk.push(12))
+print(stk.pop())
+print(stk.pop())
+print(stk.push(13), stk.push(14), stk.push(15))
+print(stk.len())
