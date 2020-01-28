076d58db4c23cd57c2cbe291fbd9b39287aea974
Frank Martinez <frankie.martinez@gmail.com>
2020-01-28 13:43:02 -0800

    ✨ Linked lists, queues

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -19,7 +19,7 @@ class ListNode:
 
     """Wrap the given value in a ListNode and insert it
     before this node. Note that this node could already
-    have a previous node it is point to."""
+    have a previous node it is pointed to."""
     def insert_before(self, value):
         current_prev = self.prev
         self.prev = ListNode(value, current_prev, self)
@@ -37,8 +37,6 @@ class ListNode:
 
 """Our doubly-linked list class. It holds references to
 the list's head and tail nodes."""
-
-
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
@@ -52,41 +50,88 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
-        
+        self.length -= 1
+        if not self.head and self.tail:
+            # TODO error handling
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head == node:
+            self.head = self.head.next
+            node.delete()
+        elif self.tail == node:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            node.delete()
+
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        # Get thehighest value in list
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head.next
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,6 +3,12 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
+# Should have the methods: enqueue, dequeue, and len.
+# enqueue should add an item to the back of the queue.
+# dequeue should remove and return an item from the front of the queue.
+# len returns the number of items in the queue.
+
+
 class Queue:
     def __init__(self):
         self.size = 0
